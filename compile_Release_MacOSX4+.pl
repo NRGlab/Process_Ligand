4a1f2a94c74d8bd9d2278eb31435f2f7ab0da57a
Francis Gaudreault <francis@Chopin.local>
2013-02-06 16:34:24 -0500

    Perl compilation scripts added for MacOSX4/5+

---
+++ compile_Release_MacOSX4+.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+$CXX = 'g++-4.0';
+$SYS = '-isysroot /Developer/SDKs/MacOSX10.4u.sdk';
+$MACHOPT = '-DMACOSX_DEPLOYMENT_TARGET=10.4 -mmacosx-version-min=10.4';
+
+
+$INCLUDE_PATH = '/usr/local/include/openbabel-2.0';
+$LIBRARY_PATH = '/Users/francisgaudreault/Development/NRGsuite/Executables/MacOSX4+';
+
+$dir = '/Users/francisgaudreault/Development/Atom_Types';
+
+`$CXX -Wall -o Process_Ligand Process_Ligand.cpp geometry.cpp -O3 -I$INCLUDE_PATH -L$LIBRARY_PATH -lopenbabel $SYS -arch i386 $MACHOPT`;
+
+`ln -fs $dir/src/Process_Ligand $dir/Process_Ligand`;
