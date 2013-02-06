c4c399019e49229159446001258ad40fbd7bc6e9
Francis Gaudreault <francis@Chopin.local>
2013-02-06 16:41:43 -0500

    Symbolic link removed in perl scripts for MacOSX4/5+

--- compile_Release_MacOSX5+.pl
+++ compile_Release_MacOSX5+.pl
@@ -8,8 +8,4 @@ $MACHOPT = '-DMACOSX_DEPLOYMENT_TARGET=10.5 -mmacosx-version-min=10.5';
 $INCLUDE_PATH = '/usr/local/include/openbabel-2.0';
 $LIBRARY_PATH = '/Users/francisgaudreault/Development/NRGsuite/Executables/MacOSX5+';
 
-$dir = '/Users/francisgaudreault/Development/Atom_Types';
-
 `$CXX -Wall -o Process_Ligand Process_Ligand.cpp geometry.cpp -O3 -I$INCLUDE_PATH -L$LIBRARY_PATH -lopenbabel $SYS -arch i386 $MACHOPT -arch x86_64 $MACHOPT`;
-
-`ln -fs $dir/src/Process_Ligand $dir/Process_Ligand`;
