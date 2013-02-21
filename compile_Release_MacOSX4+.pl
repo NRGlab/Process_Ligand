bb6d05393d58dadc6ef2b7ebeefb6e5c3760334f
Francis Gaudreault <francis@Chopin.local>
2013-02-21 10:19:43 -0500

    compile release for MacOSX+ files were adapted with new path.

--- compile_Release_MacOSX4+.pl
+++ compile_Release_MacOSX4+.pl
@@ -4,8 +4,7 @@ $CXX = 'g++-4.0';
 $SYS = '-isysroot /Developer/SDKs/MacOSX10.4u.sdk';
 $MACHOPT = '-DMACOSX_DEPLOYMENT_TARGET=10.4 -mmacosx-version-min=10.4';
 
-
-$INCLUDE_PATH = '/usr/local/include/openbabel-2.0';
-$LIBRARY_PATH = '/Users/francisgaudreault/Development/NRGsuite/Executables/MacOSX4+';
+$INCLUDE_PATH = '/Users/francisgaudreault/Programs/openbabel-2.3.2/10.4u/include/openbabel-2.0';
+$LIBRARY_PATH = '/Users/francisgaudreault/Programs/openbabel-2.3.2/10.4u/lib';
 
 `$CXX -Wall -o Process_Ligand Process_Ligand.cpp geometry.cpp -O3 -I$INCLUDE_PATH -L$LIBRARY_PATH -lopenbabel $SYS -arch i386 $MACHOPT`;
--- compile_Release_MacOSX5+.pl
+++ compile_Release_MacOSX5+.pl
@@ -5,7 +5,7 @@ $SYS = '-isysroot /Developer/SDKs/MacOSX10.5.sdk';
 $MACHOPT = '-DMACOSX_DEPLOYMENT_TARGET=10.5 -mmacosx-version-min=10.5';
 
 
-$INCLUDE_PATH = '/usr/local/include/openbabel-2.0';
-$LIBRARY_PATH = '/Users/francisgaudreault/Development/NRGsuite/Executables/MacOSX5+';
+$INCLUDE_PATH = '/Users/francisgaudreault/Programs/openbabel-2.3.2/10.5/include/openbabel-2.0';
+$LIBRARY_PATH = '/Users/francisgaudreault/Programs/openbabel-2.3.2/10.5/lib';
 
 `$CXX -Wall -o Process_Ligand Process_Ligand.cpp geometry.cpp -O3 -I$INCLUDE_PATH -L$LIBRARY_PATH -lopenbabel $SYS -arch i386 $MACHOPT -arch x86_64 $MACHOPT`;
